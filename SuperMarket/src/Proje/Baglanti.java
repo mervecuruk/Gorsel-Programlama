aa79508e5e804170e8aadb15a2fb3c17990cf3da
Merve Çürük <93490603+mervecuruk@users.noreply.github.com>
2022-01-03 16:23:32 +0300

    Add files via upload

---
+++ SuperMarket/src/Proje/Baglanti.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Proje;
+import java.sql.*;
+/**
+ *
+ * @author serdar
+ */
+public class Baglanti {
+   public static Connection getCon(){
+       try
+       {
+           Class.forName("com.mysql.jdbc.Driver");
+           Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/market","root","123cs123");
+           return con;
+       }catch(Exception e)
+       {
+           return null;
+       }
+   } 
+}
